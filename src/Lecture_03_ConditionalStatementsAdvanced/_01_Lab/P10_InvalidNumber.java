ea19218da732fa61a98ea6ef1b3dbcfe437b6e9e
Kremena <kremidimitrova0@gmail.com>
2022-06-12 01:51:16 +0300

    Commit: conditional statements advanced

---
+++ src/Lecture_03_ConditionalStatementsAdvanced/_01_Lab/P10_InvalidNumber.java
@@ -0,0 +1,16 @@
+package Lecture_03_ConditionalStatementsAdvanced._01_Lab;
+
+import java.util.Scanner;
+
+public class P10_InvalidNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        boolean isValid = (number >= 100 && number <= 200) || number == 0;
+
+        if (!isValid) {
+            System.out.println("invalid");
+        }
+    }
+}
