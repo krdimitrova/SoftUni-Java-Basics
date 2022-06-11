77c6a2363319805c401ada659f0b40bc56bf4c93
Kremena <kremidimitrova0@gmail.com>
2022-06-12 01:45:11 +0300

    Commit: conditional statements advanced

---
+++ src/Lecture_03_ConditionalStatementsAdvanced/_01_Lab/P3_AnimalType.java
@@ -0,0 +1,25 @@
+package Lecture_03_ConditionalStatementsAdvanced._01_Lab;
+
+import java.util.Scanner;
+
+public class P3_AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String animal = scanner.nextLine();
+
+        switch (animal) {
+            case "dog":
+                System.out.println("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                System.out.println("reptile");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
+}
