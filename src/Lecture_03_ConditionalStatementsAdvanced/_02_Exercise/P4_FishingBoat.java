e00e84ba1f263aed724549e816f86dc9e285fb29
Kremena <kremidimitrova0@gmail.com>
2022-06-13 19:22:57 +0300

    Commit: conditional statements advanced

---
+++ src/Lecture_03_ConditionalStatementsAdvanced/_02_Exercise/P4_FishingBoat.java
@@ -0,0 +1,48 @@
+package Lecture_03_ConditionalStatementsAdvanced._02_Exercise;
+
+import java.util.Scanner;
+
+public class P4_FishingBoat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int budget = Integer.parseInt(scanner.nextLine());
+        String season = scanner.nextLine();
+        int fishers = Integer.parseInt(scanner.nextLine());
+        double rent = 0.0;
+        double discount = 0.0;
+
+        switch (season) {
+            case "Spring":
+                rent = 3000;
+                break;
+            case "Summer":
+            case "Autumn":
+                rent = 4200;
+                break;
+            case "Winter":
+                rent = 2600;
+                break;
+        }
+
+        if (fishers <= 6) {
+            discount = 0.10;
+        } else if (fishers <= 11) {
+            discount = 0.15;
+        } else {
+            discount = 0.25;
+        }
+
+        double neededMoney = rent - rent * discount;
+        if (fishers % 2 == 0 && !season.equals("Autumn")) {
+            neededMoney *= 0.95;
+        }
+
+        if (budget >= neededMoney) {
+            System.out.printf("Yes! You have %.2f leva left.", budget - neededMoney);
+        } else {
+            System.out.printf("Not enough money! You need %.2f leva.", neededMoney - budget);
+        }
+
+    }
+}
