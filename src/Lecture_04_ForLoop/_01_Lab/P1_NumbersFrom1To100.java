e85ac0664fc404e2bb9a017dc854516cd156cf17
Kremena <kremidimitrova0@gmail.com>
2022-06-14 23:38:21 +0300

    Commit: for-loop

---
+++ src/Lecture_04_ForLoop/_01_Lab/P1_NumbersFrom1To100.java
@@ -0,0 +1,10 @@
+package Lecture_04_ForLoop._01_Lab;
+
+public class P1_NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 100; number ++) {
+            System.out.println(number);
+        }
+    }
+}
