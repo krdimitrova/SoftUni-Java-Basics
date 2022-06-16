5d46a7021e3e98171f0c7fcc9eb5fdfd2f39276f
Kremena <kremidimitrova0@gmail.com>
2022-06-17 00:30:38 +0300

    Commit: while-loop

---
+++ src/Lecture_05_WhileLoop/_02_Exercise/P4_Walking.java
@@ -0,0 +1,27 @@
+package Lecture_05_WhileLoop._02_Exercise;
+
+import java.util.Scanner;
+
+public class P4_Walking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int goal = 10000;
+        int steps = 0;
+
+        while (goal > steps) {
+            String input = scanner.nextLine();
+
+            if (input.equals("Going home")) {
+                steps += Integer.parseInt(scanner.nextLine());
+                break;
+            }
+            steps += Integer.parseInt(input);
+        }
+        if (steps >= goal) {
+            System.out.printf("Goal reached! Good job! %n%d steps over the goal!", steps - goal);
+        } else {
+            System.out.printf("%d more steps to reach goal.", goal - steps);
+        }
+    }
+}
