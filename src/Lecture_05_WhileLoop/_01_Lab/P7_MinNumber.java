2903ddaed740a4f5d1dfbe4b26e40e26b22bbfce
Kremena <kremidimitrova0@gmail.com>
2022-06-16 23:15:45 +0300

    Commit: while-loop

---
+++ src/Lecture_05_WhileLoop/_01_Lab/P7_MinNumber.java
@@ -0,0 +1,21 @@
+package Lecture_05_WhileLoop._01_Lab;
+
+import java.util.Scanner;
+
+public class P7_MinNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int min = Integer.MAX_VALUE;
+
+        while (!input.equals("Stop")) {
+            int number = Integer.parseInt(input);
+            if (number < min) {
+                min = number;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(min);
+    }
+}
