0fe3ac1c879c5bafe7b76f39e5b0face9c7294ce
Kremena <kremidimitrova0@gmail.com>
2022-06-23 23:57:50 +0300

    Commit: more exercises

---
+++ src/MoreExercises/_03_WhileLoop/P5_AverageNumber.java
@@ -0,0 +1,22 @@
+package MoreExercises._03_WhileLoop;
+
+import java.util.Scanner;
+
+public class P5_AverageNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        double count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            sum += num;
+            count++;
+        }
+
+        System.out.printf("%.2f", sum / count);
+
+    }
+}
