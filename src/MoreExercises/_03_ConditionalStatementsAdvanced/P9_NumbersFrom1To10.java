b1ec6d38bf09b7e5133f2b3869b080b00d869b07
Kremena <kremidimitrova0@gmail.com>
2022-06-27 23:57:09 +0300

    Commit: more exercises

---
+++ src/MoreExercises/_03_ConditionalStatementsAdvanced/P9_NumbersFrom1To10.java
@@ -0,0 +1,12 @@
+package MoreExercises._03_ConditionalStatementsAdvanced;
+
+public class P9_NumbersFrom1To10 {
+    public static void main(String[] args) {
+
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num ++;
+        }
+    }
+}
